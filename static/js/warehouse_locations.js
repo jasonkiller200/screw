944893c57e24a724c9584ecd817ba7c3aa729860
ralf(梁栢章) <ralf@hartford.com.tw>
2025-10-22 09:14:39 +0800

    fix: 修復onclick屬性換行導致的JavaScript語法錯誤，移除調試代碼

--- static/js/warehouse_locations.js
+++ static/js/warehouse_locations.js
@@ -5,33 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
     const EDIT_LOCATION_URL_TPL = body.dataset.editLocationUrlTpl;
     const DELETE_LOCATION_URL_TPL = body.dataset.deleteLocationUrlTpl;
 
-    // Debug: 檢查URL模板是否正確加載
-    console.log('URL Templates loaded:', {
-        EDIT_WAREHOUSE_URL_TPL,
-        DELETE_WAREHOUSE_URL_TPL,
-        EDIT_LOCATION_URL_TPL,
-        DELETE_LOCATION_URL_TPL
-    });
-
     window.editWarehouse = function(id, name, code, description) {
-        console.log('editWarehouse called:', { id, name, code, description });
         const form = document.getElementById('editWarehouseForm');
-        console.log('Form found:', form);
-        console.log('URL template:', EDIT_WAREHOUSE_URL_TPL);
-        
         if (form && EDIT_WAREHOUSE_URL_TPL) {
             form.action = EDIT_WAREHOUSE_URL_TPL.replace('0', id);
-            console.log('Form action set to:', form.action);
-            
             document.getElementById('edit_warehouse_code').value = code;
             document.getElementById('edit_warehouse_name').value = name;
             document.getElementById('edit_warehouse_description').value = description;
-            
             const modal = new bootstrap.Modal(document.getElementById('editWarehouseModal'));
             modal.show();
-            console.log('Modal should be shown');
-        } else {
-            console.error('Form or URL template not found:', { form: !!form, url: !!EDIT_WAREHOUSE_URL_TPL });
         }
     };
 
@@ -46,23 +28,13 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     window.editLocation = function(id, locationCode, description) {
-        console.log('editLocation called:', { id, locationCode, description });
         const form = document.getElementById('editLocationForm');
-        console.log('Form found:', form);
-        console.log('URL template:', EDIT_LOCATION_URL_TPL);
-        
         if (form && EDIT_LOCATION_URL_TPL) {
             form.action = EDIT_LOCATION_URL_TPL.replace('0', id);
-            console.log('Form action set to:', form.action);
-            
             document.getElementById('edit_location_code').value = locationCode;
             document.getElementById('edit_location_description').value = description || '';
-            
             const modal = new bootstrap.Modal(document.getElementById('editLocationModal'));
             modal.show();
-            console.log('Modal should be shown');
-        } else {
-            console.error('Form or URL template not found:', { form: !!form, url: !!EDIT_LOCATION_URL_TPL });
         }
     };
 
